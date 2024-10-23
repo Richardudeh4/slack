cd5b2823b1fa97147f9559055c0414ae25c4e6f5
richardudeh4 <richardudeh03@gmail.com>
2024-10-23 21:23:24 +0100

    sorting workspaces

---
+++ src/app/workspaces/workspace-header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const WorkspaceHeader = () => {
+  return (
+    <div className='flex items-center justify-between px-4 h-[49px] gap-0.5'>
+        WorkspaceHeader
+    </div>
+  )
+}
+
+export default WorkspaceHeader
--- src/app/workspaces/workspace-sidebar.tsx
+++ src/app/workspaces/workspace-sidebar.tsx
@@ -1,16 +1,34 @@
 import { useWorkspaceId } from '@/src/hooks/use-workspace-id'
 import React from 'react'
 import { useCurrentMember } from '../features/members/api/use-current-member';
-import { useCurrentUser } from '../auth/api/use-current-user';
+import { useGetWorkspace } from '../features/workspaces/api/use-get-workspace';
+import { AlertTriangle, Loader } from 'lucide-react';
+import WorkspaceHeader from './workspace-header';
 
 const WorkspaceSidebar = () => {
     const workspaceId = useWorkspaceId();
 
-    const {} = useCurrentMember({workspaceId});
-    const {} = useCurrentUser();
+    const {data: member, isLoading: memberLoading} = useCurrentMember({workspaceId});
+    const {data: workspace, isLoading: workspaceLoading} = useGetWorkspace({id: workspaceId})
+
+    if(workspaceLoading || memberLoading){
+        return (
+            <div className='flex flex-col bg-[#5E2C5F] h-full items-center justify-center'>
+                <Loader className='size-5 animate-spin text-white'/>
+            </div>
+        )
+    }
+    if(!workspace || !member ){
+        return (
+            <div className='flex flex-col bg-[#5E2C5F] h-full items-center justify-center'>
+                <AlertTriangle className='size-5 animate-spin text-white'/>
+                <p className='text-white text-sm'>Workspace not found </p>
+            </div>
+        )
+    }
   return (
-    <div>
-        WorkspaceSidebar
+    <div className='flex flex-col bg-[#5E2C5F] h-full'>
+      <WorkspaceHeader/>
     </div>
   )
 }
